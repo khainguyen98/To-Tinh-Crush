368406bc3dc20be737ee3b6a1f7cb932f63b4ff8
khainguyen98 <91797066+khainguyen98@users.noreply.github.com>
2024-02-01 09:38:15 +0700

    Update config.js
    ád

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Phượng à, đây :>',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Ngoan xinh iu của khải ơiiii :>',
+    introDesc: `Nay là 14/2 nè 
+    Chúc bé iu anh iu hơn nè
+    Quý anh nhiều hơn nè
+    Yêu em nhiều lắm`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: '8 thích anh <33',
-    btnNo: 'Không, 8 coi 5 là a trai :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao 8 lại yêu 5 <3',
-    btnReply: 'Gửi cho lão 5 hên <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu 8 à <33333333',
-    mess: '5 biết mà 🥰. Yêu 8 nhiều nhiều 😘😘',
-    messDesc: 'Nhắn tin cho tôi nhe 8 :>>.',
+    title: '14/2 nè chúc em luôn xinh đẹp nhaaaa 😙',
+    desc: 'phải luôn yêu thương anh nhiều đó nhaa ',
+    btnYes: 'dạ vâng ạ iu anh cực kỳ <33',
+    btnNo: 'không yêu anh đâu ghét :3',
+    question:'yêu anh thì đúng rùi vậy nói gì đi nào 5 <3',
+    btnReply: 'gửi cho tình yêu to lớn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu  à <33333333',
+    mess: 'anh biết mà yêu em nhiều 😘😘',
+    messDesc: 'quay lại ib anh Khải nào :>>.',
     btnAccept: 'Âu kây :3',
-    messLink: 'https://m.me/vvnuc' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/khainguyen27/
 }
